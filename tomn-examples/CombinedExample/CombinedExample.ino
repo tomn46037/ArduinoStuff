6b1a7a8968f5caa28e32dde9447a82f7d611e525
Tom Neville <tomn@sneaky.net>
2013-06-01 20:56:14 -0400

    Everything combined..
    Temp, Baro, Compass, accelerometer, gyro.. on 3 LCD displays.  All using
    only 2 pins on the Arduino.  :)

--- tomn-examples/CombinedExample/CombinedExample.ino
+++ tomn-examples/CombinedExample/CombinedExample.ino
@@ -63,7 +63,9 @@ long b5;
 short temperature;
 long pressure;
 
+// Compass
 
+#define addressCompass 0x1E //0011110b, I2C 7bit address of HMC5883
 
 
 void setup()
@@ -175,6 +177,15 @@ void setup()
   Wire.endTransmission();     // stop transmitting
 
   lcd7.setCursor(0,1);
+  lcd7.print("Init Compass..     ");
+
+  //Put the HMC5883 IC into the correct operating mode
+  Wire.beginTransmission(addressCompass); //open communication with HMC5883
+  Wire.write(0x02); //select mode register
+  Wire.write(0x00); //continuous measurement mode
+  Wire.endTransmission();
+
+  lcd7.setCursor(0,1);
   lcd7.print("Done with Init..");
 
 }
@@ -186,6 +197,7 @@ void loop()
   // Gyro
   gyro.read();
 
+  lcd0.clear();
   lcd7.clear();
   lcd7.setCursor(0,0);
 
@@ -253,6 +265,34 @@ void loop()
   lcd7.setCursor(10,0);
   lcd7.print(Zg);
 
+  //Compass
+  int x,y,z; //triple axis data
+
+  //Tell the HMC5883 where to begin reading data
+  Wire.beginTransmission(addressCompass);
+  Wire.write(0x03); //select register 3, X MSB register
+  Wire.endTransmission();
+  
+ 
+  //Read data from each axis, 2 registers per axis
+  Wire.requestFrom(addressCompass, 6);
+  if(6<=Wire.available()){
+    x = Wire.read()<<8; //X msb
+    x |= Wire.read(); //X lsb
+    z = Wire.read()<<8; //Z msb
+    z |= Wire.read(); //Z lsb
+    y = Wire.read()<<8; //Y msb
+    y |= Wire.read(); //Y lsb
+  }
+
+  lcd0.setCursor(0,1);
+  lcd0.print(x);
+
+  lcd0.setCursor(5,1);
+  lcd0.print(y);
+
+  lcd0.setCursor(10,1);
+  lcd0.print(z);
 
 
 
--- tomn-examples/CombinedExample/README.md
+++ tomn-examples/CombinedExample/README.md
@@ -2,8 +2,14 @@
 This is a combined example.  (I'm playing. :)
 
 So far, this includes the displays and the BMP085 Barometric Pressure
-sensor.
+sensor, accelerometer, gyro and compass.
 
-It counts up on display 7 and prints out the temp and pressure on the
-display at address 1.
+Display 7 - Line 1: Accelerometer
+Dispaly 7 - Line 2: Gyro
+
+Display 1 - Line 1 - Temp
+Display 1 - Line 2 - Barometric Pressure
+
+Display 0 - Line 1 - Blank
+Display 0 - Line 2 - Compass output.
 
