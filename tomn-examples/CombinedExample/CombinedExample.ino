75a25bde26ae1f134014477c0bf65bbf3e9cac7e
Tom Neville <tomn@sneaky.net>
2013-06-01 20:05:16 -0400

    Updated combined example to include the accelerometer.

--- tomn-examples/CombinedExample/CombinedExample.ino
+++ tomn-examples/CombinedExample/CombinedExample.ino
@@ -19,6 +19,23 @@ int c = 1;
 // Gyro 
 L3G4200D gyro;
 
+// Accelerometer
+#define Register_ID 0
+#define Register_2D 0x2D
+#define Register_X0 0x32
+#define Register_X1 0x33
+#define Register_Y0 0x34
+#define Register_Y1 0x35
+#define Register_Z0 0x36
+#define Register_Z1 0x37
+
+int ADXAddress = 0xA7 >> 1;  // the default 7-bit slave address
+int reading = 0;
+int val=0;
+int X0,X1,X_out;
+int Y0,Y1,Y_out;
+int Z1,Z0,Z_out;
+double Xg,Yg,Zg;
 
 
 // Now we setup for the Barometric Pressure sensor.
@@ -150,6 +167,14 @@ void setup()
   gyro.enableDefault();
 
   lcd7.setCursor(0,1);
+  lcd7.print("Init Accel..     ");
+
+  Wire.beginTransmission(ADXAddress);
+  Wire.write(Register_2D);
+  Wire.write(8);                //measuring enable
+  Wire.endTransmission();     // stop transmitting
+
+  lcd7.setCursor(0,1);
   lcd7.print("Done with Init..");
 
 }
@@ -158,6 +183,7 @@ void loop()
 {
   delay(100);
 
+  // Gyro
   gyro.read();
 
   lcd7.clear();
@@ -171,6 +197,63 @@ void loop()
 
   lcd7.setCursor(10,1);
   lcd7.print((int)gyro.g.z);
+
+  //Accel
+  //--------------X
+  Wire.beginTransmission(ADXAddress); // transmit to device
+  Wire.write(Register_X0);
+  Wire.write(Register_X1);
+  Wire.endTransmission();
+  Wire.requestFrom(ADXAddress,2); 
+  if(Wire.available()<=2)   
+  {
+    X0 = Wire.read();
+    X1 = Wire.read(); 
+    X1=X1<<8;
+    X_out=X0+X1;   
+  }
+
+  //------------------Y
+  Wire.beginTransmission(ADXAddress); // transmit to device
+  Wire.write(Register_Y0);
+  Wire.write(Register_Y1);
+  Wire.endTransmission();
+  Wire.requestFrom(ADXAddress,2); 
+  if(Wire.available()<=2)   
+  {
+    Y0 = Wire.read();
+    Y1 = Wire.read(); 
+    Y1=Y1<<8;
+    Y_out=Y0+Y1;
+  }
+  //------------------Z
+  Wire.beginTransmission(ADXAddress); // transmit to device
+  Wire.write(Register_Z0);
+  Wire.write(Register_Z1);
+  Wire.endTransmission();
+  Wire.requestFrom(ADXAddress,2); 
+  if(Wire.available()<=2)   
+  {
+    Z0 = Wire.read();
+    Z1 = Wire.read(); 
+    Z1=Z1<<8;
+    Z_out=Z0+Z1;
+  }
+  //
+  Xg=X_out/256.0;
+  Yg=Y_out/256.0;
+  Zg=Z_out/256.0;
+
+  lcd7.setCursor(0,0);
+  lcd7.print(Xg);
+
+  lcd7.setCursor(5,0);
+  lcd7.print(Yg);
+
+  lcd7.setCursor(10,0);
+  lcd7.print(Zg);
+
+
 
 
   if ( c%100 ) {  
